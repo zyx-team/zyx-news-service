585b88dca58bae5af339e9b3132493e00f26b863
zhangyaxin <yxzhang6@163.com>
2021-07-03 23:24:19 +0800

    修改ignore文件

---
+++ zyx-news-service-admin/src/main/java/com/zyx/admin/repository/FriendLinkRepository.java
@@ -0,0 +1,12 @@
+package com.zyx.admin.repository;
+
+import com.zyx.pojo.mo.FriendLinkMO;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface FriendLinkRepository extends MongoRepository<FriendLinkMO, String> {
+
+    public List<FriendLinkMO> getAllByIsDelete(Integer isDelete);
+
+}
