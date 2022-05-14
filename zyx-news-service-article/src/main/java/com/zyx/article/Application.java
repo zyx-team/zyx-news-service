b33d00eb9fd0f055d059415a0d122a9abf5246bb
yxzhang6 <yxzhang6@163.com>
2022-05-14 23:12:12 +0800

    升级为nacos注册中心-成功！！！

--- zyx-news-service-article/src/main/java/com/zyx/article/Application.java
+++ zyx-news-service-article/src/main/java/com/zyx/article/Application.java
@@ -3,6 +3,7 @@ package com.zyx.article;
 import com.rule.MyRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
@@ -13,7 +14,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
 @MapperScan(basePackages = "com.zyx.article.mapper")
 @ComponentScan(basePackages = {"com.zyx", "org.n3r.idworker"})
-@EnableEurekaClient
+@EnableDiscoveryClient
 //@RibbonClient(name = "SERVICE-USER", configuration = MyRule.class)
 @EnableFeignClients({"com.zyx"})
 @EnableHystrix
